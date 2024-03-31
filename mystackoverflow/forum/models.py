873de56386cc725d1517a8f1cbf16d5ec2a210c1
drakonbr <45700302+drakonbr@users.noreply.github.com>
2024-03-31 15:33:55 -0300

    instalando Django

---
+++ mystackoverflow/forum/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+import datetime
+from django.utils import timezone
+
+class Question(models.Model):
+    title = models.CharField(max_length=200, null=False)
+    details = models.TextField(null=False)
+    have_tried = models.TextField()
+    created_date = models.DateTimeField("Created on")
+    def __str__(self):
+        return "[" + str(self.id) + "] " + self.title
+    
+    def was_published_recently(self):
+        return self.created_date >= timezone.now() - datetime.timedelta(days=1)
+
+
